7052b59324dcce1be347a85c485669775d62b63e
CJ <pinedacj1020@gmail.com>
2022-08-14 02:07:28 +0800

    Added user choice prompt

--- script.js
+++ script.js
@@ -4,6 +4,26 @@ function getComputerChoice(){
     return rps[choice];
 }
 function playRound(playerSelection, computerSelection){
-    
+    x = playerSelection.toLowerCase();
+    y = computerSelection;
+
+    if(x == y){
+        console.log("Tie");
+    }else if (x == "rock" && y == "paper"){
+        console.log("You Lose! Paper beats Rock");
+    }else if (x == "rock" && y == "scissors"){
+        console.log("You Win! Rock beats Scissors");
+    }else if (x == "paper" && y == "scissors"){
+        console.log("You Lose! Scissors beats Paper");
+    }else if (x == "paper" && y == "rock"){
+        console.log("You Win! Paper beats Rock")
+    }else if (x == "scissors" && y == "rock"){
+        console.log("You Lose! Rock beats Scissors");
+    }else if (x == "scissors" && y == "paper"){
+        console.log("You Win! Scissors beats Paper");
+    }
 }
-getComputerChoice()
+
+y = getComputerChoice();
+x = prompt("Rock Paper Scissors!");
+playRound(x,y);
