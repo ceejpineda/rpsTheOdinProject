fc46cd87a43695fff18de3267547d06ec0ac816a
CJ <pinedacj1020@gmail.com>
2022-08-14 01:56:13 +0800

    Added computer rps random code

---
+++ script.js
@@ -0,0 +1,9 @@
+function getComputerChoice(){
+    rps = ["rock","paper","scissors"];
+    choice = Math.floor(Math.random()*rps.length);
+    return rps[choice];
+}
+function playRound(playerSelection, computerSelection){
+    
+}
+getComputerChoice()
